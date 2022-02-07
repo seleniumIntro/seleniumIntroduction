e012126823685684e3fe1b8409303e8301b989d3
Alexander <alexander.dobychin@foodrocket.me>
2022-02-08 02:18:42 +0300

    add dimension

--- src/main/java/DriverManager.java
+++ src/main/java/DriverManager.java
@@ -1,11 +1,23 @@
+import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+import java.util.concurrent.TimeUnit;
 
 public class DriverManager {
 
     private static WebDriver wd;
     private static void setDriver(){
         wd = new ChromeDriver(WebDriverConfig.configChrome());
+        //неявное ожидание
+        //wd.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        wd.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        wd.manage().window().maximize();
+
+         //задать разрешение
+//        Dimension dimension = new Dimension(480,700);
+//        wd.manage().window().setSize(dimension);
     }
 
     public  static WebDriver getDriver(){
--- src/test/java/Tests.java
+++ src/test/java/Tests.java
@@ -1,17 +1,42 @@
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 
 public class Tests extends WdBase{
 
-    @Test
-    public void firstTest() {
+    public void auth(){
         wd.get("https://www.mail.ru");
         wd.findElement(By.className("email-input")).sendKeys("selenium.introduction");
         wd.findElement(By.xpath("//*[@id=\"mailbox\"]/form[1]/button[1]")).click();
-        //wd.findElement(By.linkText("Ввести пароль")).click();
-
         wd.findElement(By.xpath("//input[@data-testid=\"password-input\"]")).sendKeys("qwerty@2020");
         wd.findElement(By.xpath("//button[@data-testid=\"login-to-mail\"]")).click();
+    }
+
+    @Test
+    public void firstTest() {
+        auth();
+
+        //id
+        //wd.findElement(By.xpath("//button[@data-testid=\"login-to-mail\"]")).click();
+        //className
+        //cssSelectoer
+        //partLink
+
+    }
+
+    @Test
+    public void waitTest() {
+        wd.get("https://www.yandex.ru");
+        // По-другому это можно сделать так:
+        // driver.navigate().to("https://www.yandex.ru");
+
+        //явное ожидание
+//        WebElement element = (new WebDriverWait(wd, Duration.ofSeconds(10))
+//                .until(ExpectedConditions.presenceOfElementLocated(By.id("123"))));
 
     }
 }
